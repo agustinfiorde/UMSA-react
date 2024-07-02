ed992e6e95c591ed68f97fae3874adc9fc58a776
agustinfiorde <agulirio@yahoo.com.ar>
2024-07-02 13:37:40 -0300

    Progress on example

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -2,13 +2,12 @@ import { Box } from "@mui/material";
 import { useRickAndMorty } from "../hooks/RickAndMortyContext";
 
 const Page2: React.FC = () => {
-  const { characters, episodes, locations } = useRickAndMorty();
+  const { characters  } = useRickAndMorty();
 
   return (
     <Box>
       <Box>{JSON.stringify(characters, null, 2)}</Box>
-      <Box>{JSON.stringify(episodes, null, 2)}</Box>
-      <Box>{JSON.stringify(locations, null, 2)}</Box>
+
     </Box>
   );
 };
--- src/pages/Page3.tsx
+++ src/pages/Page3.tsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
+import { useRickAndMorty } from "../hooks/RickAndMortyContext";
 
 const Page3: React.FC = () => {
-  
-    return (
-      <Box>holis page 3</Box>
-    );
-  };
-  
-  export default Page3;
-  
+  const { episodes, locations } = useRickAndMorty();
+
+  return (
+    <Box>
+      <Box>{JSON.stringify(episodes, null, 2)}</Box>
+      <Box>{JSON.stringify(locations, null, 2)}</Box>
+    </Box>
+  );
+};
+
+export default Page3;
